db09f890acecacabe14cf200a1d6f50da0323fda
parkerOffice <69760520+psmithdev@users.noreply.github.com>
2023-07-26 15:03:48 -0500

    Onmouseover event listener to change div color pink

--- sketchpad.js
+++ sketchpad.js
@@ -2,6 +2,12 @@ for (let i = 0; i < 4; i++) {
   for (let j = 0; j < 4; j++) {
     const div = document.createElement("div");
     div.className = "square";
+
+    div.onmouseover = function (event) {
+      let target = event.target;
+      target.style.background = "pink";
+    };
+
     const container = document.getElementsByClassName("container")[0];
     container.appendChild(div);
   }
