482072aad972894da85a6daf10a64d5681659174
parkerOffice <69760520+psmithdev@users.noreply.github.com>
2023-07-27 12:18:10 -0500

    Calculate square size and set container width

--- sketchpad.js
+++ sketchpad.js
@@ -1,10 +1,17 @@
 function createGrid(size) {
   const container = document.getElementsByClassName("container")[0];
   container.innerHTML = "";
+
+  let containerStyle = window.getComputedStyle(container);
+  let containerWidth = parseFloat(containerStyle.getPropertyValue("width"));
+  let squareSize = containerWidth / size;
+
   for (let i = 0; i < size; i++) {
     for (let j = 0; j < size; j++) {
       const div = document.createElement("div");
       div.className = "square";
+      div.style.width = `${squareSize}px`;
+      div.style.height = `${squareSize}px`;
 
       div.onmouseover = function (event) {
         let target = event.target;
@@ -20,6 +27,10 @@ function resetMe() {
   let popUp = prompt("Please enter number of squares per side for new grid");
   if (popUp === null || isNaN(popUp)) {
     console.log("not valid input");
+  }
+  if (popUp >= 100) {
+    console.log("That value is too large, will crash computer");
+    return;
   }
   createGrid(popUp);
 }
